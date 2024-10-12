89804464eff1fe6c57fa2a1b8f62e9fb20e4b05e
Swan Htet Aung <swanhtetaung308@gmail.com>
2024-10-12 23:49:34 +0630

    social login test

---
+++ src/main/java/com/learning/social/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.learning.social.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> helloPage(){
+        return ResponseEntity.ok("Hello From Oauth2");
+    }
+}
